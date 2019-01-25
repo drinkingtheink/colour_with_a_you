670b01c989adef86169cacc7a50cbe2daaf603e3
Jason M Harrison <harrisonj@dnb.com>
2019-01-25 12:33:13 -0600

    Selecting random color and displaying it is up and runnning.

---
+++ src/components/ActiveColor.vue
@@ -0,0 +1,27 @@
+<template>
+	<div 
+		class="current_color_display"
+		:style="{ backgroundColor: this.color }"
+	>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ActiveColor',
+		props: {
+			color: String
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.current_color_display {
+		margin-top: 1rem;
+		height: 3em;
+		width: 100%;
+		border: 5px solid #333;
+		transition: all .2s;
+	}
+</style>
--- src/components/ColorSelector.vue
+++ src/components/ColorSelector.vue
@@ -5,13 +5,20 @@
     <button class="select_random_color" @click="getRandomColor()">
       Select One at Random
     </button>
+
+    <ActiveColor v-show="selectedColor" :color="this.selectedColor"/>
   </div>
 </template>
 
 <script>
 import randomColor from 'randomcolor'
+import ActiveColor from './ActiveColor.vue'
+
 export default {
   name: 'ColorSelector',
+  components: {
+    ActiveColor
+  },
   data () {
     return {
       selectedColor: null
